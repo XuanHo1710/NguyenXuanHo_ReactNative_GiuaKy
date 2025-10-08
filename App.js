fe41a56f92502a6aac6e7d135ee93c070677b9d8
H82M19\Student <Student>
2025-10-08 07:40:22 +0700

    4. Finish done cau 4 a,b

--- App.js
+++ App.js
@@ -1,48 +1,66 @@
-
-import React, { useState, useEffect, useMemo, useCallback, useRef } from "react";
-import { 
-  SafeAreaView, 
-  Text, 
-  View, 
-  FlatList, 
-  TextInput, 
-  Button, 
-  ActivityIndicator, 
-  RefreshControl 
-} from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+} from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  RefreshControl,
+  Switch,
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
 export default function App() {
   // ---------------- STATE ----------------
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
-  const [newTodo, setNewTodo] = useState("");
+  const [newTodo, setNewTodo] = useState('');
   const [editingId, setEditingId] = useState(null);
-  const [editingText, setEditingText] = useState("");
+  const [editingText, setEditingText] = useState('');
   const inputRef = useRef(null);
 
   // ---------------- API BASE ----------------
-  const API_URL = "https://jsonplaceholder.typicode.com/todos";
+  const API_URL = 'https://6832918dc3f2222a8cb2b453.mockapi.io/api/v1/products';
 
   // ---------------- FETCH DATA (GET) ----------------
   const fetchTodos = useCallback(async () => {
+    await axios.get(API_URL).then((res) => setTodos(res.data));
     // TODO: gọi API GET và cập nhật state
   }, []);
 
   useEffect(() => {
     // TODO: gọi fetchTodos khi mở app
-  }, []);
+    fetchTodos();
+  }, [fetchTodos]);
 
   // ---------------- ADD (POST) ----------------
   const addTodo = useCallback(async () => {
+    await axios.post(API_URL, {
+      name: inputRef.current.value,
+      completed: false
+    }).then(res => res.status === 201 ? alert("Add success") : alert("Add fail"))
+    .finally(() =>  {
+       inputRef.current.clear(); 
+       onRefresh();
+    })
     // TODO: gọi API POST và thêm công việc mới
-  }, [newTodo]);
+  }, [newTodo]); 
 
   // ---------------- UPDATE (PUT) ----------------
-  const saveEdit = useCallback(async (id) => {
-    // TODO: gọi API PUT để cập nhật công việc
-  }, [editingText]);
+  const saveEdit = useCallback(
+    async (id) => {
+      // TODO: gọi API PUT để cập nhật công việc
+    },
+    [editingText]
+  );
 
   // ---------------- DELETE ----------------
   const deleteTodo = useCallback(async (id) => {
@@ -52,6 +70,7 @@ export default function App() {
   // ---------------- REFRESH ----------------
   const onRefresh = useCallback(async () => {
     // TODO: gọi fetchTodos khi refresh
+    fetchTodos();
   }, [fetchTodos]);
 
   // ---------------- MEMO LIST ----------------
@@ -59,9 +78,46 @@ export default function App() {
 
   // ---------------- RENDER ITEM ----------------
   const renderItem = ({ item }) => (
-    <View style={{ flexDirection: "row", justifyContent: "space-between", padding: 10 }}>
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        alignItems: 'center',
+      }}>
       {/* TODO: nếu đang edit thì hiển thị TextInput, ngược lại hiển thị Text */}
       {/* TODO: thêm nút Save / Edit / Delete */}
+      {editingId === null ? (
+        <Text lineBreakMode={'tail'} numberOfLines={1}>
+          {item.name}
+        </Text>
+      ) : (
+        <TextInput style={{borderWidth: 1, padding: 5, width: "70%"}} value={item.name} />
+      )}
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+        {editingId === null ? (
+          <>
+            <Switch value={item.completed} />
+            <Button
+              onPress={() => {
+                 setEditingId(item.id);
+                 setEditingText(item.name)
+              }}
+              color="black"
+              title="Edit"
+            />
+            <Button color="black" title="Delete" />
+          </>
+        ) : (
+          <Button
+            onPress={() => {
+              setEditingId(null);
+            }}
+            color="black"
+            title="Save"
+          />
+        )}
+      </View>
     </View>
   );
 
@@ -69,12 +125,12 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, margin: 10 }}>
-        <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
           MidTerm React Native – Todo App
         </Text>
 
         {/* Input thêm mới */}
-        <View style={{ flexDirection: "row", marginBottom: 10 }}>
+        <View style={{ flexDirection: 'row', marginBottom: 10 }}>
           <TextInput
             ref={inputRef}
             placeholder="Enter new todo..."
