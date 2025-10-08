196494769664ab8236a39fbe07c31753f9319825
H82M19\Student <Student>
2025-10-08 07:42:36 +0700

    4.1 Push code

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import {
   Switch,
 } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import TaskItem from './components/TaskItem';
 import axios from 'axios';
 export default function App() {
   // ---------------- STATE ----------------
@@ -23,7 +24,6 @@ export default function App() {
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
   const [newTodo, setNewTodo] = useState('');
-  const [editingId, setEditingId] = useState(null);
   const [editingText, setEditingText] = useState('');
   const inputRef = useRef(null);
 
@@ -43,29 +43,51 @@ export default function App() {
 
   // ---------------- ADD (POST) ----------------
   const addTodo = useCallback(async () => {
-    await axios.post(API_URL, {
-      name: inputRef.current.value,
-      completed: false
-    }).then(res => res.status === 201 ? alert("Add success") : alert("Add fail"))
-    .finally(() =>  {
-       inputRef.current.clear(); 
-       onRefresh();
-    })
+    await axios
+      .post(API_URL, {
+        name: inputRef.current.value,
+        completed: false,
+      })
+      .then((res) =>
+        res.status === 201 ? alert('Add success') : alert('Add fail')
+      )
+      .finally(() => {
+        inputRef.current.clear();
+        onRefresh();
+      });
     // TODO: gọi API POST và thêm công việc mới
-  }, [newTodo]); 
+  }, [onRefresh]);
 
   // ---------------- UPDATE (PUT) ----------------
   const saveEdit = useCallback(
-    async (id) => {
+    async (item) => {
       // TODO: gọi API PUT để cập nhật công việc
+      await axios
+        .put(API_URL + '/' + item.id, {
+          name: item.name,
+          completed: item.completed,
+        })
+        .then((res) =>
+          res.status === 200 ? alert('Edit success') : alert('Edit fail')
+        )
+        .finally(() => onRefresh());
     },
-    [editingText]
+    [onRefresh]
   );
 
   // ---------------- DELETE ----------------
-  const deleteTodo = useCallback(async (id) => {
-    // TODO: gọi API DELETE và xoá công việc
-  }, []);
+  const deleteTodo = useCallback(
+    async (id) => {
+      // TODO: gọi API DELETE và xoá công việc
+      await axios
+        .delete(API_URL + '/' + id)
+        .then((res) =>
+          res.status === 200 ? alert('Remove success') : alert('Remove fail')
+        )
+        .finally(() => onRefresh());
+    },
+    [onRefresh]
+  );
 
   // ---------------- REFRESH ----------------
   const onRefresh = useCallback(async () => {
@@ -77,49 +99,7 @@ export default function App() {
   const todoList = useMemo(() => todos, [todos]);
 
   // ---------------- RENDER ITEM ----------------
-  const renderItem = ({ item }) => (
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 10,
-        alignItems: 'center',
-      }}>
-      {/* TODO: nếu đang edit thì hiển thị TextInput, ngược lại hiển thị Text */}
-      {/* TODO: thêm nút Save / Edit / Delete */}
-      {editingId === null ? (
-        <Text lineBreakMode={'tail'} numberOfLines={1}>
-          {item.name}
-        </Text>
-      ) : (
-        <TextInput style={{borderWidth: 1, padding: 5, width: "70%"}} value={item.name} />
-      )}
-      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-        {editingId === null ? (
-          <>
-            <Switch value={item.completed} />
-            <Button
-              onPress={() => {
-                 setEditingId(item.id);
-                 setEditingText(item.name)
-              }}
-              color="black"
-              title="Edit"
-            />
-            <Button color="black" title="Delete" />
-          </>
-        ) : (
-          <Button
-            onPress={() => {
-              setEditingId(null);
-            }}
-            color="black"
-            title="Save"
-          />
-        )}
-      </View>
-    </View>
-  );
+  // Import tu components
 
   // ---------------- UI ----------------
   return (
@@ -147,7 +127,9 @@ export default function App() {
           <FlatList
             data={todoList}
             keyExtractor={(item) => item.id.toString()}
-            renderItem={renderItem}
+            renderItem={({ item }) => (
+              <TaskItem item={item} saveEdit={saveEdit} />
+            )}
             refreshControl={
               <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
             }
---
+++ TaskItem.js
@@ -0,0 +1,63 @@
+import {
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  RefreshControl,
+  Switch,
+} from 'react-native';
+
+import React, {useState} from 'react'
+
+const TaskItem = ({ item, saveEdit }) => {
+  const [editingId, setEditingId] = useState(null);
+  const [name, setName] = useState(item.name);
+
+  return (
+     <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        alignItems: 'center',
+      }}>
+      {/* TODO: nếu đang edit thì hiển thị TextInput, ngược lại hiển thị Text */}
+      {/* TODO: thêm nút Save / Edit / Delete */}
+      {editingId === null ? (
+        <Text lineBreakMode={'tail'} numberOfLines={1}>
+          {item.name}
+        </Text>
+      ) : (
+        <TextInput onChangeText={setName} style={{borderWidth: 1, padding: 5, width: "70%"}} value={name} />
+      )}
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+        {editingId === null ? (
+          <>
+            <Switch value={item.completed} />
+            <Button
+              onPress={() => {
+                 setEditingId(item.id);
+              }}
+              color="black"
+              title="Edit"
+            />
+            <Button color="black" title="Delete" />
+          </>
+        ) : (
+          <Button
+            onPress={() => {
+              saveEdit({id: item.id, name: name, completed: item.completed})
+              setEditingId(null);
+            }}
+            color="black"
+            title="Save"
+          />
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default React.memo(TaskItem);
